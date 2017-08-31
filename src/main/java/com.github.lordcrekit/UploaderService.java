97d3c625d431b6ac1ef33e3e4f78533bc9f0cfde
William Norman <lordcrekit@gmail.com>
2017-08-30 23:59:05 -0400

    Working on the UploaderService

--- src/main/java/com.github.lordcrekit/UploaderService.java
+++ src/main/java/com.github.lordcrekit/UploaderService.java
@@ -1,29 +1,60 @@
 package com.github.lordcrekit;
 
+import org.json.JSONObject;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 /**
  * The UploadService is responsible for queuing the upload of files.
+ * <p>
+ * It controls one thread and communicates with it through ZMQ.PUSH Sockets.
  *
  * @author William A. Norman <a href="norman.william.dev@gmail.com/>
  */
 public class UploaderService implements Closeable {
+
+  private final class UploaderServiceThread implements Runnable {
+
+    private final ZContext context;
+    private final String address;
+
+    final UploaderServiceThread(final ZContext context, final String address) {
+      this.context = context;
+      this.address = address;
+    }
+
+    @Override
+    public void run() {
+
+    }
+  }
+
+  private final ZContext context;
 
   private final String address;
   private final UploaderCache cache;
   private final UploaderRouter router;
   private final UploaderStrategy strategy;
 
+  private Thread thread;
+
   /**
    * Create a new UploadService.
    */
-  public UploaderService(UploaderCache cache,
-                         UploaderRouter router,
-                         UploaderStrategy strategy) {
+  public UploaderService(final ZContext context,
+                         final UploaderCache cache,
+                         final UploaderRouter router,
+                         final UploaderStrategy strategy) {
+
+    this.context = context;
 
     this.address = "inproc://"
         + UploaderService.class.getName()
@@ -36,6 +67,10 @@ public class UploaderService implements Closeable {
   }
 
   public void start() {
+    if (this.thread == null) {
+      this.thread = new Thread(new UploaderServiceThread(this.context, this.address));
+      this.thread.start();
+    }
   }
 
   /**
@@ -46,13 +81,48 @@ public class UploaderService implements Closeable {
    * @param destination
    *     The destination to upload to.
    */
-  public void queueUpload(Path file, URI destination) {
+  public void queueUpload(final Path file, final URI destination) {
+    final ZMQ.Socket sock = this.context.createSocket(ZMQ.PUSH);
+    try {
+      sock.connect(this.address);
+      final JSONObject msg = new JSONObject();
+      msg.put("c", 1);
+      msg.put("f", file.normalize().toString());
+      msg.put("d", destination.normalize().toString());
+
+      sock.send(msg.toString());
+    } finally {
+      context.destroySocket(sock);
+    }
   }
 
   /**
+   * Queue a file for upload. The service's {@link UploaderRouter} determines locations to upload to.
+   *
    * @param file
+   *     The file to upload.
    */
-  public void queueUpload(Path file) {
+  public void queueUpload(final Path file) {
+    final List<URI> destinations = this.router.route(file);
+    if (destinations.isEmpty()) {
+      return;
+    }
+
+    final ZMQ.Socket sock = this.context.createSocket(ZMQ.PUSH);
+    try {
+      sock.connect(this.address);
+
+      for (URI uri : destinations) {
+        final JSONObject msg = new JSONObject();
+        msg.put("c", 1);
+        msg.put("f", file.normalize().toString());
+        msg.put("d", uri.normalize().toString());
+
+        sock.send(msg.toString());
+      }
+    } finally {
+      context.destroySocket(sock);
+    }
   }
 
   /**
@@ -60,16 +130,36 @@ public class UploaderService implements Closeable {
    * as soon as possible, call {@link #close()}.
    */
   public void terminate() {
+    final ZMQ.Socket sock = this.context.createSocket(ZMQ.PUSH);
+    try {
+      sock.connect(this.address);
+      final JSONObject msg = new JSONObject();
+      msg.put("c", -1);
+
+      sock.send(msg.toString());
+    } finally {
+      context.destroySocket(sock);
+    }
   }
 
   /**
    * Wait for the UploadService to terminate naturally. It will complete every upload asked of it before it was asked to
    * terminate.
+   *
+   * @throws InterruptedException
+   *     If the thread awaiting termination is interrupted.
    */
-  public void awaitTermination() {
+  public void awaitTermination() throws InterruptedException {
+    this.thread.join();
   }
 
   @Override
   public void close() throws IOException {
+    this.terminate();
+    this.thread.interrupt();
+    try {
+      this.awaitTermination();
+    } catch (InterruptedException e) {
+      throw new IOException("Do not interrupt thread closing UploaderService.", e);
   }
 }
--- src/test/java/com.github.lordcrekit/UploaderServiceTest.java
+++ src/test/java/com.github.lordcrekit/UploaderServiceTest.java
@@ -6,34 +6,32 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.zeromq.ZContext;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
 /**
  * @author William A. Norman <a href="norman.william.dev@gmail.com/>
  */
-public class UploadServiceTest {
+public class UploaderServiceTest {
 
   private static ZContext CONTEXT;
   private static Path TEST_DIRECTORY;
 
   @BeforeClass
   public static void setUp() throws IOException {
-    System.out.println(UploadServiceTest.class.getName());
+    System.out.println(UploaderServiceTest.class.getName());
 
     CONTEXT = new ZContext();
     TEST_DIRECTORY = Files.createTempDirectory(
-        UploadServiceTest.class.getName());
+        UploaderServiceTest.class.getName());
   }
 
   @AfterClass
@@ -82,31 +80,31 @@ public class UploadServiceTest {
   @Test
   public void testManyChangesPolicy()
       throws IOException {
-
     System.out.println("\tTest changing faster than uploading");
 
-    final Path cacheFile = Files.createTempFile(TEST_DIRECTORY, UploadServiceTest.class.getName(), "");
-    try {
-      final AtomicReference<Long> fileDate = new AtomicReference<Long>(new Long(1));
-      final UploaderCache cache = new UploaderCache(CONTEXT, cacheFile);
-      final TestingStrategy strategy = new TestingStrategy(fileDate);
-      final UploaderService service = new UploaderService(cache, new TestingRouter(), strategy);
+    final Path cacheFile = Files.createTempFile(TEST_DIRECTORY, "cache", ".json");
+    final Path uploadPath = Files.createTempFile(TEST_DIRECTORY, "test", ".txt");
+    final TestingStrategy strategy = new TestingStrategy();
+    try (final UploaderCache cache = new UploaderCache(CONTEXT, cacheFile);
+         final UploaderService service = new UploaderService(cache, new TestingRouter(), strategy)) {
 
-      final Path uploadPath = Paths.get(TEST_DIRECTORY.toString(), "test.txt");
+      Files.write(uploadPath, "data".getBytes());
       for (int i = 2; i < 10; i++) {
+        Files.setLastModifiedTime(uploadPath, FileTime.fromMillis(i * 1000));
         service.queueUpload(uploadPath);
-        fileDate.set(new Long(i));
         Thread.sleep(TimeUnit.SECONDS.toMillis(1) / 20);
       }
 
       service.terminate();
       service.awaitTermination();
+
       Assert.assertEquals(2, strategy.Count); // It should have only needed to upload twice. Or once maybe.
 
     } catch (InterruptedException e) {
       Assert.fail("Error in testing code. Should not have been interrupted.");
     } finally {
       Files.delete(cacheFile);
+      Files.delete(uploadPath);
     }
   }
 
@@ -121,6 +119,8 @@ public class UploadServiceTest {
          final UploaderService service = new UploaderService(cache, new TestingRouter(), strategy)) {
 
       cache.ignore(Pattern.compile(".*\\.ignore.*"));
+
+      Files.write(uploadFile, "data".getBytes());
 
       service.queueUpload(uploadFile);
       service.terminate();
@@ -137,21 +137,29 @@ public class UploadServiceTest {
   public void testFrozenFiles() throws IOException {
     System.out.println("\tTest frozen files");
 
-    final Path cacheFile = Files.createTempFile(TEST_DIRECTORY, UploadServiceTest.class.getName(), "");
+    final Path cacheFile = Files.createTempFile(TEST_DIRECTORY, UploaderServiceTest.class.getName(), "");
     final Path toUpload = Files.createTempFile(TEST_DIRECTORY, ".freeze", "");
 
     final TestingStrategy strategy = new TestingStrategy();
     try (final UploaderCache cache = new UploaderCache(CONTEXT, cacheFile);
          final UploaderService service = new UploaderService(cache, new TestingRouter(), strategy)) {
 
-      cache.ignore(Pattern.compile(".*\\.freeze.*"));
+      Files.write(toUpload, "data".getBytes());
+      Files.setLastModifiedTime(toUpload, FileTime.fromMillis(50));
+
+      cache.freeze(Pattern.compile(".*\\.freeze.*"), 70);
       service.queueUpload(toUpload);
+
+      Files.setLastModifiedTime(toUpload, FileTime.fromMillis(80));
+      service.queueUpload(toUpload);
+
       service.terminate();
       service.awaitTermination();
 
-      Assert.assertEquals(0, strategy.Count);
+      Assert.assertEquals(1, strategy.Count);
     } finally {
       Files.delete(cacheFile);
+      Files.delete(toUpload);
     }
   }
 }
