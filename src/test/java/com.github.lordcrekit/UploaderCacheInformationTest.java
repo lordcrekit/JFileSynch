7816f228ec3a82f57cf2f6af275c87c878529e4f
William Norman <lordcrekit@gmail.com>
2017-09-07 22:24:17 -0400

    Finished unit tests for UploaderCacheInformation

--- src/test/java/com.github.lordcrekit/UploaderCacheInformationTest.java
+++ src/test/java/com.github.lordcrekit/UploaderCacheInformationTest.java
@@ -3,12 +3,17 @@ package com.github.lordcrekit;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
 public class UploaderCacheInformationTest {
 
   @Test
   public void testEquals() {
+    System.out.println("Test equals(OBJECT)");
     final UploaderCacheInformation o1 = new UploaderCacheInformation();
     final UploaderCacheInformation o2 = new UploaderCacheInformation();
 
@@ -19,6 +24,10 @@ public class UploaderCacheInformationTest {
     o1.IgnoredPatterns.add(Pattern.compile(".*"));
     Assert.assertNotEquals(o1, o2);
     Assert.assertNotEquals(o2, o1);
+    o2.IgnoredPatterns.add(Pattern.compile("[a-d].*"));
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.IgnoredPatterns.clear();
     o2.IgnoredPatterns.add(Pattern.compile(".*"));
     Assert.assertEquals(o1, o2);
 
@@ -30,20 +39,81 @@ public class UploaderCacheInformationTest {
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Frozen patterns">
-    Assert.fail("todo");
+    o1.FrozenPatterns.put(Pattern.compile(".*"), (long) 50);
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.FrozenPatterns.put(Pattern.compile("[a-d].*"), (long) 50);
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.FrozenPatterns.clear();
+    o2.FrozenPatterns.put(Pattern.compile(".*"), (long) 50);
+    Assert.assertEquals(o1, o2);
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Frozen timestamps">
-    Assert.fail("todo");
+    o1.TimestampsWhenFrozen.put(Paths.get("xD"), (long) 60);
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.TimestampsWhenFrozen.put(Paths.get("xD"), (long) 70);
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.TimestampsWhenFrozen.clear();
+    o2.TimestampsWhenFrozen.put(Paths.get("zXD"), (long) 60);
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.TimestampsWhenFrozen.clear();
+    o2.TimestampsWhenFrozen.put(Paths.get("xD"), (long) 60);
+    Assert.assertEquals(o1, o2);
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Timestamps">
-    Assert.fail("todo");
+    o1.Timestamps.put(Paths.get("xD"), (long) 60);
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.Timestamps.put(Paths.get("xD"), (long) 70);
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.Timestamps.clear();
+    o2.Timestamps.put(Paths.get("zXD"), (long) 60);
+    Assert.assertNotEquals(o1, o2);
+    Assert.assertNotEquals(o2, o1);
+    o2.Timestamps.clear();
+    o2.Timestamps.put(Paths.get("xD"), (long) 60);
+    Assert.assertEquals(o1, o2);
     // </editor-fold>
   }
 
   @Test
-  public void testIO() {
-    final UploaderCacheInformation o;
+  public void testIO() throws IOException {
+    final UploaderCacheInformation o1 = new UploaderCacheInformation();
+    UploaderCacheInformation o2;
+
+    // Empty
+    o2 = new UploaderCacheInformation(o1.toJSON());
+    Assert.assertEquals(o1, o2);
+
+    // <editor-fold defaultstate="collapsed" desc="Ignored patterns">
+    o1.IgnoredPatterns.add(Pattern.compile(".*"));
+    o2 = new UploaderCacheInformation(o1.toJSON());
+    Assert.assertEquals(o1, o2);
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Frozen patterns">
+    o1.FrozenPatterns.put(Pattern.compile("[a-z]*"), (long) 60);
+    o2 = new UploaderCacheInformation(o1.toJSON());
+    Assert.assertEquals(o1, o2);
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Frozen timestamps">
+    o1.TimestampsWhenFrozen.put(Paths.get("xD"), (long) 60);
+    o2 = new UploaderCacheInformation(o1.toJSON());
+    Assert.assertEquals(o1, o2);
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Timestamps">
+    o1.Timestamps.put(Paths.get("lol"), (long) 60);
+    o2 = new UploaderCacheInformation(o1.toJSON());
+    Assert.assertEquals(o1, o2);
+    // </editor-fold>
   }
 }
